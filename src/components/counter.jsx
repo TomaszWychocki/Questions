952c0ccd3fbc20fea116d5641d5897e2b0048a72
Tomasz Wychocki <wychoniu@gmail.com>
2019-02-18 17:30:17 +0100

    react init

---
+++ src/components/counter.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    state ={
+        count: 0
+    };
+
+    handleClick = () => {
+        this.setState({count: this.state.count + 1});
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <span>{this.state.count}</span>
+                <button onClick={this.handleClick}>Click</button>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Counter;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './components/counter';
+import Button from './components/button';
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Button />, document.getElementById('root'));
